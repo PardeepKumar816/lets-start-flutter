2c98013f943ce668c8d8e8a9a0528143cfb3ee0f
PardeepKumar816 <pardeepmalhi816@gmail.com>
2022-10-23 21:42:13 +0500

    resolved issue in onlong_pressed_gesture

--- code_example/lib/LongPress_Gesture.dart
+++ code_example/lib/LongPress_Gesture.dart
@@ -1,17 +1,38 @@
-class _MyCardWidgetState extends State<MyCardWidget> { 
-Color bgColor = Colors.yellow; 
-bool makeCircular = false; 
+import 'package:flutter/material.dart';
 
-@override Widget build(BuildContext context) { 
+class MyCardWidget extends StatefulWidget {
+  const MyCardWidget({Key? key}) : super(key: key);
 
-return GestureDetector( 
-	onLongPress: (){ 
-		setState(() { makeCircular = !makeCircular; }); }, 
-	child: Card( shape: makeCircular? 
-			const CircleBorder(): const RoundedRectangleBorder(), 
-		child: const SizedBox( height: 300, width: 300, ), 
-		color: bgColor, 
-		), 
-	); 
-} 
-} 
+  @override
+  State<MyCardWidget> createState() => _MyCardWidgetState();
+}
+
+class _MyCardWidgetState extends State<MyCardWidget> {
+  Color bgColor = Colors.yellow;
+  bool makeCircular = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onLongPress: () {
+        setState(() {
+          makeCircular = !makeCircular;
+        });
+      },
+      child: Card(
+        shape: makeCircular
+            ? const CircleBorder()
+            : const RoundedRectangleBorder(),
+        color: bgColor,
+        child: const SizedBox(
+          height: 300,
+          width: 300,
+        ),
+      ),
+    );
+  }
+}
+
+// class _MyCardWidgetState extends State<MyCardWidget> {
+//
+// }
