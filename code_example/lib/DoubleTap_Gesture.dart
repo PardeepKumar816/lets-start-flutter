5b7ff5c07d9f173c0147e96b293e68c0724240b1
PardeepKumar816 <pardeepmalhi816@gmail.com>
2022-10-23 21:38:30 +0500

    resolved issue in double_tab_gesture

--- code_example/lib/DoubleTap_Gesture.dart
+++ code_example/lib/DoubleTap_Gesture.dart
@@ -1,15 +1,37 @@
-class _MyCardWidgetState extends State<MyCardWidget> { 
-Color bgColor = Colors.yellow; 
-@override Widget build(BuildContext context) { 
-return GestureDetector( 
-	onDoubleTap: (){ 
-		setState(() { 
-		bgColor = Colors.primaries[Random().nextInt(Colors.primaries.length)]; 
-		}); 
-	}, 
-	child: Card( child: const SizedBox( height: 300, width: 300, ), 
-		color: bgColor, 
-		), 
-	); 
-} 
-} 
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class MyCardWidget extends StatefulWidget {
+  const MyCardWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyCardWidget> createState() => _MyCardWidgetState();
+}
+
+class _MyCardWidgetState extends State<MyCardWidget> {
+  Color bgColor = Colors.yellow;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onDoubleTap: () {
+        setState(() {
+          bgColor = Colors.primaries[Random().nextInt(Colors.primaries.length)];
+        });
+      },
+      child: Card(
+        color: bgColor,
+        child: const SizedBox(
+          height: 300,
+          width: 300,
+        ),
+      ),
+    );
+  }
+}
+
+// class _MyCardWidgetState extends State<MyCardWidget> {
+//
+// @override Widget build(BuildContext context) {
+// return
+// }
